fcdfe22eef6e35b93ccf59dd4de733e4cc00d986
czarlos <cer26@duke.edu>
2014-11-26 19:23:59 -0500

    hidden paragraphs working

--- js/parallax.js
+++ js/parallax.js
@@ -1,11 +1,8 @@
 $(document).ready(function() {
-	
-	//redrawDotNav();
-	
+		
 	/* Scroll event handler */
     $(window).bind('scroll',function(e){
     	parallaxScroll();
-		redrawDotNav();
     });
     
 	/* Next/prev and primary nav btn click handlers */
@@ -57,11 +54,12 @@ $(document).ready(function() {
     	console.log("HASDKL");
     });
 
- 	$("#hello").click(function(){
- 		console.log("hello");
-            $("#test").slideToggle("slow", function(){
-            	console.log("text");
-            });
+ 	$(".gallery").click(function(event){
+
+            var elementID = "#"+event.target.id+"-paragraph";
+
+            $(elementID).slideToggle("slow");
+
         });
 
     /* Animate transitions */
@@ -91,22 +89,3 @@ function parallaxScroll(){
 	$('#parallax-bg3').css('top',(0-(scrolled*.75))+'px');
 }
 
-/* Set navigation dots to an active state as the user scrolls */
-// function redrawDotNav(){
-// 	var section1Top =  0;
-// 	// The top of each section is offset by half the distance to the previous section.
-// 	var section2Top =  $('#frameless-parachute').offset().top - (($('#english-channel').offset().top - $('#frameless-parachute').offset().top) / 2);
-// 	var section3Top =  $('#english-channel').offset().top - (($('#about').offset().top - $('#english-channel').offset().top) / 2);
-// 	var section4Top =  $('#about').offset().top - (($(document).height() - $('#about').offset().top) / 2);;
-// 	$('nav#primary a').removeClass('active');
-// 	if($(document).scrollTop() >= section1Top && $(document).scrollTop() < section2Top){
-// 		$('nav#primary a.manned-flight').addClass('active');
-// 	} else if ($(document).scrollTop() >= section2Top && $(document).scrollTop() < section3Top){
-// 		$('nav#primary a.frameless-parachute').addClass('active');
-// 	} else if ($(document).scrollTop() >= section3Top && $(document).scrollTop() < section4Top){
-// 		$('nav#primary a.english-channel').addClass('active');
-// 	} else if ($(document).scrollTop() >= section4Top){
-// 		$('nav#primary a.about').addClass('active');
-// 	}
-	
-// }
